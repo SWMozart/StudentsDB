90a1c5872551fa312dbd4c017764f19bdaeb74d4
sebastianwitt <witt.seb@outlook.de>
2022-09-07 14:58:40 +0200

    Add HashApp and Remove

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        StudentDB studentArchiv = new StudentDB();
+
         CSStudent student1 = new CSStudent(123, "Andy" , "Java");
+        CSStudent student2 = new CSStudent(443, "Sandy" , "C++");
+        CSStudent student3 = new CSStudent(987, "Randy" , "Java");
 
-        System.out.println(student1);
 
+        studentArchiv.add(student1);
+        studentArchiv.add(student2);
+        studentArchiv.add(student3);
+
+        studentArchiv.remove(student1.getId());
+        studentArchiv.remove(student2.getId());
+
+        System.out.println(studentArchiv);
 
 
     }
--- src/main/java/repo/StudentDB.java
+++ src/main/java/repo/StudentDB.java
@@ -2,23 +2,25 @@ package repo;
 
 import model.Student;
 
+import java.util.HashMap;
+
 public class StudentDB {
 
-    private Student[] students;
-
-    public StudentDB(Student[] students){
-        this.students = students;
+    HashMap<String, Student> students = new HashMap<>();
+    public Student add(Student student) {
+        students.put(student.getId(), student);
+        return student;
+    }
+    public void remove(String id) {
+        students.remove(id);
     }
 
-    public Student[] getAllStudents(){
-        return this.students;
-    }
 
     @Override
     public String toString() {
         String dbAsString = "";
 
-        for (Student student: students) {
+        for (Student student: students.values()) {
             dbAsString = dbAsString + " " + students.toString();
         }
 
